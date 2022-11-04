97fbdd4997606ae02e94d7f2479ade3b41eb36ed
Promise Anendah <anendahpromise@gmail.com>
2022-11-04 11:31:55 +0100

    Update user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,33 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  name: {
-    required: true,
-    type: String
+    name: {
+      required: true,
+      type: String
+    },
+
+    phone_number: String,
+    
+    email: {
+      type: String,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    user_type: {
+      type: String,
+      enum: ["user", "author"],
+      required: true
+    }
   },
-});
+  {
+    timestamps: true
+  }
+);
 
 const User = mongoose.model('users', schema);
 
